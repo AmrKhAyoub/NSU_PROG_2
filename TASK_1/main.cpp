9a971d58f05de9c8788c8c9207633faf8c107000
amrkhayoub <amrkh2004njr@gmail.com>
2026-09-08 19:10:08 +0700

    Task 1 - Ph1Pt2 Implement memory allocation and deallocation methods in Matrix class

--- TASK_1/main.cpp
+++ TASK_1/main.cpp
@@ -7,6 +7,32 @@ class Matrix {
     private:
         int size;
         int** data;
+
+        void allocateMemory(int n) {
+            size = n;
+            if (size == 0) {
+                data = nullptr;
+                return;
+            }
+            // Allocate array of pointers
+            data = new int*[size];
+            for (int i = 0; i < size; ++i) {
+                // Allocate rows
+                data[i] = new int[size];
+            }
+        }
+
+        void freeMemory() {
+            if (data != nullptr) {
+                for (int i = 0; i < size; ++i) {
+                    delete[] data[i];
+                }
+                delete[] data;
+                data = nullptr;
+            }
+            size = 0;
+        }
+
 };
 
 int main(){
